e588d46058af249782ffc1426cd69ded10be87e0
Zidar <gabriel.saganic@gmail.com>
2020-01-29 22:24:26 +0100

    opet sve

---
+++ CMakeFiles/rak_drogon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rak_drogon.dir/main.cc.o"
+  "rak_drogon"
+  "rak_drogon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rak_drogon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
